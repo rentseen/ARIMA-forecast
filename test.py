d8e4125ebcd45e6deeec2023bad97e6284ce3fb2
yelinsheng <yelinmgl@hotmail.com>
2016-11-02 18:42:16 +0800

    result plot

--- test.py
+++ test.py
@@ -16,14 +16,17 @@ a.x=-1
 print(a.x)
 print(b[0].x)
 '''
+'''
 import matplotlib.pyplot as plt
-f=open("result.txt","r")
+f=open("C-static.txt","r")
+out=open("C-CDF-static.txt","w")
 x=f.read()
 x=x.split('\n')
 y=x[:100]
 for i in range(100):
 	y[i]=int(y[i])
 y.sort()
+'''
 '''p=open("sort.txt","w")
 for i in range(100):
 	y[i]=str(y[i])
@@ -31,19 +34,34 @@ for i in range(100):
 	p.write(y[i])
 	p.write("\n")
 '''
+'''
 m=[]
 n=[]
 m.append(y[0])
-n.append(1)
+n.append(0.01)
 length=0
 for i in range(1,100):
 	if(m[length]==y[i]):
-		n[length]=n[length]+1
+		n[length]=n[length]+0.01
 	else:
 		m.append(y[i])
-		n.append(1)
+		n.append(n[length]+0.01)
 		length=length+1
 print m
 print n
+for i in range(len(m)):
+	out.write(str(m[i]))
+	out.write("\t")
+	out.write(str(n[i]))
+	out.write("\n")
+out.close()
 plt.plot(m,n)
 plt.show()
+'''
+f1=open("C-CDF-static.txt","r")
+f2=open("C-CDF-CAVMP.txt","r")
+f3=open("C-CDF-CAstatic.txt","r")
+
+x1=f1.read()
+x1=x1.split('\n')
+y1=x1[:100]
